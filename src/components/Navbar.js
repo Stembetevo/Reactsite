c242a734137c37f3341cd1122b8a56d319480f3c
Steve <stephenkaruru05@gmail.com>
2023-06-24 12:58:07 +0300

    react info site

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <div>
+        <nav>
+           <img src="img/reacticon.jpg"/>
+           <h2 className="nav_logo">ReactFacts</h2>
+           <h3 className="nav_title">React Course-Project 1</h3>
+        </nav>
+        </div>
+    )
+}
