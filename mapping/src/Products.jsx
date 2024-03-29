d91a75c9451f7239f45e0405affc72f64f632647
Steka <stephenkaruru05@gmail.com>
2024-03-30 00:00:30 +0300

    props

--- mapping/src/Products.jsx
+++ mapping/src/Products.jsx
@@ -3,8 +3,8 @@ import React from "react";
    function Products(props){
     return(
         <div>
-           <h2>{props.productsdata.name}</h2>
-           <p>{props.productsdata.price}--{props.productsdata.description}</p>
+           <h2>{props.Products.name}</h2>
+           <p>{props.Products.price}--{props.Products.description}</p>
         </div>
     )
    }
